d2a7862135583d9377cb7565d5cf75b4b62a5217
raghavs2k <sharma.raghav@razorpay.com>
2025-02-25 16:02:08 +0530

    packages and variable added

--- hello-world.go
+++ hello-world.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
-	fmt.Println("hello world 2")
+	fmt.Println("Hello, World!")
+	fmt.Println("My favorite number is", rand.Intn(10))
+	var x int = 10
+	y := 1000
+	var a = true
+	fmt.Println(x, y, a)
 }
