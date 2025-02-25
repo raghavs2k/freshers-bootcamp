6e2972b09265bcba4f0bba2afda2762eae033ba0
raghavs2k <sharma.raghav@razorpay.com>
2025-02-25 16:07:31 +0530

    values added

--- hello-world.go
+++ hello-world.go
@@ -12,4 +12,10 @@ func main() {
 	y := 1000
 	var a = true
 	fmt.Println(x, y, a)
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("go" + "lang")
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
 }
