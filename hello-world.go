8013c7bc9a4936171546e255b83c7fa3b0d49ce6
raghavs2k <sharma.raghav@razorpay.com>
2025-02-26 11:07:54 +0530

    if-statements

--- hello-world.go
+++ hello-world.go
@@ -2,9 +2,22 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 )
 
+func sqrt(x float64) string {
+	if x < 0 {
+		return sqrt(-x) + "i"
+	}
+	return fmt.Sprint(math.Sqrt(x))
+}
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	}
+	return lim
+}
 func main() {
 	fmt.Println("Hello, World!")
 	fmt.Println("My favorite number is", rand.Intn(10))
@@ -18,4 +31,21 @@ func main() {
 	fmt.Println(true && false)
 	fmt.Println(true || false)
 	fmt.Println(!true)
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println("sum:", sum)
+	sum2 := 1
+	for sum2 < 1000 {
+		sum2 += sum2
+	}
+
+	fmt.Println("sum2:", sum2)
+	fmt.Println(sqrt(2), sqrt(-4))
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
+
 }
